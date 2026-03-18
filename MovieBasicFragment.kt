24ce1d067aca0076c6301ce3ac2212d86c7939af
Hemnath2508 <230701115@rajalakshmi.edu.in>
2026-03-18 12:44:27 +0530

    Add MovieBasicFragment class for movie display

---
+++ MovieBasicFragment.kt
@@ -0,0 +1,18 @@
+package com.example.moviefragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MovieBasicFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie_basic, container, false)
+    }
+}
